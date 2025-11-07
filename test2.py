6dddff1c319f7c33ccffe481148e6ce04494e1c8
atoz03 <xiaqingtao2015@outlook.com>
2025-11-07 16:48:18 +0800

    commit by x

--- test2.py
+++ test2.py
@@ -1,4 +1,13 @@
 import torch
-
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
+import torchvision.utils as utils
+import torchvision.transforms.functional as TF
+import torchvision.transforms.functional as F
 x=torch.randn(5,3)
 print(x)
